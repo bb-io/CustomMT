6d3fa6419727aaf6be8701cdde8162d608dd8784
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 14:04:31 +0300

    added categories

--- Apps.CustomMT/CustomMtApplication.cs
+++ Apps.CustomMT/CustomMtApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.CustomMT;
 
-public class CustomMtApplication : IApplication
+public class CustomMtApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.MachineTranslationAndMtqe];
+        set { }
+    }
+    
     public string Name
     {
         get => "Custom MT";
